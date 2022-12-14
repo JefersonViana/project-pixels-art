9e9ba0ceebdcffd63acff284e5e62f0445e857ea
jefersonviana <jefersonv28@gmail.com>
2022-12-14 18:55:46 -0300

    Corrigindo erros

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+window.onload = () => {
 const body = document.querySelector('body');
 const titulo = document.createElement('h1');
 body.appendChild(titulo);
@@ -5,7 +6,7 @@ titulo.innerText = 'Paleta de Cores';
 titulo.id = 'title';
 
 const section = document.createElement('section');
-section.id = 'color-palette'
+section.id = 'color-palette';
 body.appendChild(section);
 const button = document.createElement('button');
 body.appendChild(button);
@@ -17,9 +18,6 @@ for (let index = 0; index < 4; index += 1) {
   section.appendChild(div);
   div.className = 'color';
 }
-button.addEventListener('click', randomColors);
-
-
 
 function randomColors() {
   const string = '0123456789ABCDEF';
@@ -27,11 +25,14 @@ function randomColors() {
   const newColor = document.querySelectorAll('.color').length;
   for (let index = 1; index < newColor; index += 1) {
     const colorDiv = document.querySelectorAll('.color')[index];
-
+    
     for (let index1 = 0; index1 < 6; index1 += 1) {
       corGenerate += string[Math.floor(Math.random() * 16)];
       colorDiv.style.background = corGenerate;
     }
     corGenerate = '#';
   }
+}
+
+button.addEventListener('click', randomColors);
 }
