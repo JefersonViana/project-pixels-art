1b66b27d0b3b6edc2f175da76f9402c234825b41
jefersonviana <jefersonv28@gmail.com>
2022-12-14 18:32:06 -0300

    Requisitos 3 e 4  concluídos

--- script.js
+++ script.js
@@ -7,9 +7,31 @@ titulo.id = 'title';
 const section = document.createElement('section');
 section.id = 'color-palette'
 body.appendChild(section);
+const button = document.createElement('button');
+body.appendChild(button);
+button.innerText = 'Cores aleatórias';
+button.id = 'button-random-color';
 
 for (let index = 0; index < 4; index += 1) {
   const div = document.createElement('div');
   section.appendChild(div);
   div.className = 'color';
+}
+button.addEventListener('click', randomColors);
+
+
+
+function randomColors() {
+  const string = '0123456789ABCDEF';
+  let corGenerate = '#';
+  const newColor = document.querySelectorAll('.color').length;
+  for (let index = 1; index < newColor; index += 1) {
+    const colorDiv = document.querySelectorAll('.color')[index];
+
+    for (let index1 = 0; index1 < 6; index1 += 1) {
+      corGenerate += string[Math.floor(Math.random() * 16)];
+      colorDiv.style.background = corGenerate;
+    }
+    corGenerate = '#';
+  }
 }
