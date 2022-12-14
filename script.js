ee5521ecc28cf0b0c0078906bed84cd12fea850b
jefersonviana <jefersonv28@gmail.com>
2022-12-14 16:31:04 -0300

    Requisito 2 concluído

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const body = document.querySelector('body');
 const titulo = document.createElement('h1');
 body.appendChild(titulo);
-titulo.innerText = "Paleta de Cores";
-titulo.id = "title"
+titulo.innerText = 'Paleta de Cores';
+titulo.id = 'title';
+
+const section = document.createElement('section');
+section.id = 'color-palette'
+body.appendChild(section);
+
+for (let index = 0; index < 4; index += 1) {
+  const div = document.createElement('div');
+  section.appendChild(div);
+  div.className = 'color';
+}
